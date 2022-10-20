a62ea2c619b6df1a4e2fca71b351db49dcdc2a2c
liJianHong19720904 <59433358+liJianHong19720904@users.noreply.github.com>
2022-10-20 22:27:21 +0800

    Delete showExps/person directory

--- showExps/person/fbs.js
+++
@@ -1,2 +0,0 @@
-var personName = ["Bill Gates","Jeff Bezos","Warren Buffett","Mark Zucerberg", "Larry Ellison","Charles Koch","David Koch","Michael Bloomberg","Larry Page","Sergey Brin"] ;
-var personInfo = ["MicroSoft 创始人","Amason 创始人","股神 第一人","FaceBook 创始人","Oracle 创始人","此信息 尚不知情","此信息 尚不知情","彭博社 创始人","Google 创始人之一","此人信息未知"] ;
